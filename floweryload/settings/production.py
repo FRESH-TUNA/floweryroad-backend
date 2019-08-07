b38662d9e29d0b13a479abd0b2a5566b8825a4af
DONGWON KIM <lunacircle4@gmail.com>
2019-08-08 00:44:43 +0900

    nginx, uwsgi 을 활용하여 ec2 에 배포 연습

---
+++ floweryload/settings/production.py
@@ -0,0 +1,15 @@
+from .base import *
+
+DEBUG = False
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    }
+}
+
+ALLOWED_HOSTS = [
+    'ec2-13-125-213-84.ap-northeast-2.compute.amazonaws.com',
+]
+
