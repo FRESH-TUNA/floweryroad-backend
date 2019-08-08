08d5b5ff7a91ccbf2c05a001baf48f68c510c28d
DONGWON KIM <lunacircle4@gmail.com>
2019-08-09 01:23:57 +0900

    feat: modify development settings for using ngrok

--- floweryroad/settings/development.py
+++ floweryroad/settings/development.py
@@ -8,15 +8,18 @@ DEBUG = True
 SECRET_KEY = '3w#j4id+z(%si4cl+7)mu%rb5wx9)0l5w5ormf+4p(zn&7q!!s'
 
 ROOT_URLCONF = 'floweryroad.urls.development'
-
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
-}
-
 #media 설정
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 MEDIA_URL = '/media/'
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'floweryroaddev',
+        'USER': 'floweryroadadmin',
+        'PASSWORD': 'floweryroad1234',
+        'HOST': '0.tcp.ngrok.io',
+        'PORT': '',
+    }
+}
