1c1659dc81e0aa137e0c449e513f2915e7a196d1
bookfairy <libronia1415@gmail.com>
2019-08-08 22:16:26 +0900

    feat: api-purposes with ViewSet

--- core/views/purpose.py
+++ core/views/purpose.py
@@ -1,13 +1,8 @@
-from rest_framework import viewsets
+from rest_framework import viewsets, mixins
 from rest_framework.response import Response
 from core.models import Purpose
 from core.serializers import PurposeSerializer
 
-class PurposeViewSet(viewsets.ViewSet):
+class PurposeViewSet(mixins.ListModelMixin, viewsets.GenericViewSet):
     queryset = Purpose.objects.all()
     serializer_class = PurposeSerializer
-
-    def list(self, request):
-        queryset = Purpose.objects.all()
-        serializer = PurposeSerializer(queryset, many=True)
-        return Response(serializer.data)
