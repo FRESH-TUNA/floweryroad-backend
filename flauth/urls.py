7937397c1bbad46b0a9e072dcbf97ad45fe739ca
bookfairy <libronia1415@gmail.com>
2019-08-09 11:43:15 +0900

    refactor: urls

--- flauth/urls.py
+++ flauth/urls.py
@@ -1,13 +1,12 @@
 from rest_framework.routers import DefaultRouter
 from rest_framework_simplejwt.views import (TokenRefreshView, TokenVerifyView,)
-from django.urls import path
+from django.urls import path, include
 from .views import signup, signinView, UserViewSet
 
 router = DefaultRouter()
 router.register(r'users', UserViewSet, basename='user')
-urlpatterns = router.urls
 
-urlpatterns += [
+urlpatterns = [
     path('signin', signinView.as_view(), name='token_obtain_pair'),
     path('signup', signup),
     path('token/refresh', TokenRefreshView.as_view(), name='token_refresh'),
