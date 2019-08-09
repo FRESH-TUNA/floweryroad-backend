fee46dd2cf44493be341d140c89ff99909cd72fd
bookfairy <libronia1415@gmail.com>
2019-08-10 03:06:10 +0900

    chore: modify detail of flower

--- core/serializers/flower.py
+++ core/serializers/flower.py
@@ -30,8 +30,7 @@ class _FlowerSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = Flower
-        fields = ['id', 'name', 'description',
-                  'season', 'languages', 'colors', 'purposes']
+        fields = ['id', 'name', 'purposes', 'image', 'star', ]
 
     def get_star(self, obj):
         return obj.comments.aggregate(avgs=Avg(F('star'))).get('avgs', None)
@@ -42,7 +41,7 @@ class FlowerListSerializer(_FlowerSerializer):
 
     class Meta:
         model = Flower
-        fields = ['id', 'name', 'purposes', 'image', 'star',]
+        fields = ['id', 'name', 'purposes', 'image', 'star', ]
 
     def get_thumbnail(self, obj):
         image = ImageSerializer(obj.images.first(), context={
@@ -55,8 +54,8 @@ class FlowerDetailSerializer(_FlowerSerializer):
 
     class Meta:
         model = Flower
-        fields = ['id', 'name', 'description', 'images',
-                  'season', 'languages', 'colors', 'purposes']
+        fields = ['id', 'name', 'description', 'star', 'season',
+                  'images', 'languages', 'colors', 'purposes']
 
 
 class CommentFlowerSerializer(FlowerListSerializer):
