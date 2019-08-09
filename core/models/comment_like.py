bc527069ae7e54c5be7dbf46b75750bcb67e9ea5
DONGWON KIM <lunacircle4@gmail.com>
2019-08-09 11:54:17 +0900

    feat: implement commentlike model prototype

--- core/models/comment_like.py
+++ core/models/comment_like.py
@@ -1,16 +1,11 @@
 import floweryroad.settings.base as settings
 from django.db import models
 from django.core.validators import MaxValueValidator, MinValueValidator
-from . import Purpose, Color, Language, Flower
+from . import Comment
 
 
-class Comment(models.Model):
+class CommentLike(models.Model):
     user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
-    flower = models.ForeignKey(Flower, on_delete=models.CASCADE)
-    content = models.CharField(max_length=200, blank=True)
-    star = models.FloatField(validators=[MinValueValidator(0.0), MaxValueValidator(5.0)], default=0.0)
-    created_at = models.DateTimeField(auto_now=True)
-
-    @property
-    def like(self):
-        return self.likes.objects.count()
+    comment = models.ForeignKey(Comment, on_delete=models.CASCADE)
+    like = models.BooleanField(default=False)
+    liked_at = models.DateTimeField(auto_now=True)
