da1d802201d25d66707b4aaab2bf6c5b48c73b96
bookfairy <libronia1415@gmail.com>
2019-08-09 18:24:41 +0900

    feat: general search

--- core/views/flower.py
+++ core/views/flower.py
@@ -1,12 +1,22 @@
-from rest_framework import viewsets, mixins
+from rest_framework import viewsets, mixins, filters
 from rest_framework.response import Response
 from core.models import Flower
 from core.serializers import FlowerListSerializer, FlowerDetailSerializer
 
 
+class FlowerFilterBackend(filters.BaseFilterBackend):
+    allowed_fields = ['query']
+    
+
+
+
 class FlowerViewSet(viewsets.ReadOnlyModelViewSet):
     queryset = Flower.objects.all()
     serializer_class = FlowerListSerializer
+    filter_backends = [filters.SearchFilter]
+
+    # 전체 검색
+    search_fields = ['name', 'description', 'languages__name', 'purposes__name']
 
     def get_serializer_class(self):
         if self.action == 'list':
