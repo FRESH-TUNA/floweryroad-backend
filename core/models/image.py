9d1ef5b29d81c5e37fb4825e971c114837abf802
bookfairy <libronia1415@gmail.com>
2019-08-09 16:52:54 +0900

    feat: api flowers

--- core/models/image.py
+++ core/models/image.py
@@ -1,6 +1,11 @@
 from django.db import models
+from django.conf import settings
 from . import Flower
 
 class Image(models.Model):
     url = models.ImageField()
-    flower = models.ForeignKey(Flower, on_delete=models.SET_NULL, null=True, blank=True)
+    flower = models.ForeignKey(Flower, related_name="images", on_delete=models.SET_NULL, null=True, blank=True)
+
+    @property
+    def image_url(self):
+        return self.url.url
--- core/serializers/flower.py
+++ core/serializers/flower.py
@@ -1,6 +1,13 @@
+# from django.http import request
 from rest_framework import serializers
-from core.models import Color, Language, Flower
+from core.models import Color, Language, Image, Flower
 from core.serializers import PurposeSerializer
+
+
+class ImageSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Image
+        fields = ['url']
 
 
 class ColorSerializer(serializers.ModelSerializer):
@@ -16,11 +23,12 @@ class LanguageSerializer(serializers.ModelSerializer):
 
 
 class FlowerSerializer(serializers.ModelSerializer):
+    images = ImageSerializer(many=True, read_only=True)
     languages = LanguageSerializer(many=True, read_only=True)
     colors = ColorSerializer(many=True, read_only=True)
     purposes = PurposeSerializer(many=True, read_only=True)
 
     class Meta:
         model = Flower
-        fields = ['id', 'name', 'description',
+        fields = ['id', 'name', 'description', 'images',
                   'season', 'languages', 'colors', 'purposes']
