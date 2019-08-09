18c3498b439a861a30eefa3c9997cb6da0b9e1a8
bookfairy <libronia1415@gmail.com>
2019-08-09 20:44:20 +0900

    feat: filtering for flowers

--- core/views/flower.py
+++ core/views/flower.py
@@ -1,22 +1,45 @@
-from rest_framework import viewsets, mixins, filters
+from rest_framework import viewsets, mixins
 from rest_framework.response import Response
+from rest_framework.filters import SearchFilter, OrderingFilter
+import django_filters
+from django_filters.rest_framework import DjangoFilterBackend
 from core.models import Flower
 from core.serializers import FlowerListSerializer, FlowerDetailSerializer
 
 
-class FlowerFilterBackend(filters.BaseFilterBackend):
-    allowed_fields = ['query']
-    
+class FlowerFilter(django_filters.FilterSet):
+    name = django_filters.CharFilter(method='icontains_filter')
+    languages = django_filters.CharFilter(method='name_filter')
+    purposes = django_filters.CharFilter(method='name_filter')
 
+    class Meta:
+        model = Flower
+        fields = ['name', 'season', 'languages', 'colors', 'purposes']
+
+    def icontains_filter(self, queryset, key, value):
+        key += '__icontains'
+        return queryset.filter(**{key: value, })
+
+    def name_filter(self, queryset, key, value):
+        key += '__name'
+        return queryset.filter(**{key: value, })
 
 
 class FlowerViewSet(viewsets.ReadOnlyModelViewSet):
     queryset = Flower.objects.all()
     serializer_class = FlowerListSerializer
-    filter_backends = [filters.SearchFilter]
+    # filter_backends = (SearchFilter, DjangoFilterBackend, OrderingFilter)
+    filter_backends = (SearchFilter, DjangoFilterBackend,)
 
-    # 전체 검색
-    search_fields = ['name', 'description', 'languages__name', 'purposes__name']
+    # 전체 검색(icontains)
+    search_fields = ['name', 'description',
+                     'languages__name', 'purposes__name']
+
+    # 특정 필드 검색
+    filter_class = FlowerFilter
+
+    # 정렬 기준
+    # ordering =('id',)
 
     def get_serializer_class(self):
         if self.action == 'list':
