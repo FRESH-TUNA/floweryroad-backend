6ac8665465d5b3d08e080ca27f530c077da2efc7
bookfairy <libronia1415@gmail.com>
2019-08-10 03:18:00 +0900

    refactor: notion styles

--- flauth/serializers/user.py
+++ flauth/serializers/user.py
@@ -1,7 +1,11 @@
 from rest_framework import serializers
 from flauth.models import User
+from core.serializers import ColorSerializer, PurposeSerializer
 
 class UserSerializer(serializers.ModelSerializer):
+    color = ColorSerializer()
+    purpose = PurposeSerializer()
+
     class Meta:
         model = User
         fields = ['id', 'nickname', 'email', 'color', 'purpose', 'date_joined']
