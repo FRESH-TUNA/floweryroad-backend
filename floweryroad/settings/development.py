ac1ce2a5467bc15974eaa6e8a60bdd6bd5c28779
DONGWON KIM <lunacircle4@likelion.org>
2019-08-28 02:04:18 +0900

    feat: dockerize dev

--- floweryroad/settings/development.py
+++ floweryroad/settings/development.py
@@ -1,12 +1,12 @@
 from .base import *
-import json
+import os
 DEBUG = True
 # Database
 # https://docs.djangoproject.com/en/2.2/ref/settings/#databases
 
-CONFIG_SECRET_DIR = os.path.join(BASE_DIR, '.credential')
-CONFIG_SETTINGS_COMMON_FILE = os.path.join(CONFIG_SECRET_DIR, 'devDBCredential.json')
-config_devdb_secret = json.loads(open(CONFIG_SETTINGS_COMMON_FILE).read())
+# CONFIG_SECRET_DIR = os.path.join(BASE_DIR, '.credential')
+# CONFIG_SETTINGS_COMMON_FILE = os.path.join(CONFIG_SECRET_DIR, 'devDBCredential.json')
+# config_devdb_secret = json.loads(open(CONFIG_SETTINGS_COMMON_FILE).read())
 
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = '3w#j4id+z(%si4cl+7)mu%rb5wx9)0l5w5ormf+4p(zn&7q!!s'
@@ -20,11 +20,11 @@ MEDIA_URL = 'http://localhost:8000/media/'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': config_devdb_secret['db']['NAME'],
-        'USER': config_devdb_secret['db']['USER'],
-        'PASSWORD': config_devdb_secret['db']['PASSWORD'],
-        'HOST': config_devdb_secret['db']['HOST'],
-        'PORT': config_devdb_secret['db']['PORT'],
+        'NAME': os.environ['DEV_DB_NAME'],
+        'USER': os.environ['DEV_DB_USERNAME'],
+        'PASSWORD': os.environ['DEV_DB_PASSWORD'],
+        'HOST': os.environ['DEV_DB_HOST'],
+        'PORT': os.environ['DEV_DB_PORT']
     }
 }
 
