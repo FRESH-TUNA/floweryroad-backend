94fe589a2821b182306d270437d38b4330532a3f
DONGWON KIM <lunacircle4@likelion.org>
2019-08-28 19:18:02 +0900

    feat: dockerfile docker-compose for production

--- floweryroad/settings/docker_production.py
+++ floweryroad/settings/docker_production.py
@@ -1,14 +1,10 @@
 from django.conf import settings
 from .base import *
-import os, json
+import os
 
-CONFIG_SECRET_DIR = os.path.join(BASE_DIR, '.credential')
-CONFIG_SETTINGS_COMMON_FILE = os.path.join(CONFIG_SECRET_DIR, 'credentials.json')
-config_secret = json.loads(open(CONFIG_SETTINGS_COMMON_FILE).read())
+DEBUG = True
 
-DEBUG = False
-
-SECRET_KEY = config_secret['SECRET_KEY']
+SECRET_KEY = os.environ['SECRET_KEY']
 
 ROOT_URLCONF = 'floweryroad.urls.docker_production'
 
@@ -23,25 +19,15 @@ CORS_ORIGIN_WHITELIST = [
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': config_secret['db']['NAME'],
-        'USER': config_secret['db']['USER'],
-        'PASSWORD': config_secret['db']['PASSWORD'],
-        'HOST': config_secret['db']['HOST'],
-        'PORT': config_secret['db']['PORT'],
+        'NAME': os.environ['DB_NAME'],
+        'USER': os.environ['DB_USERNAME'],
+        'PASSWORD': os.environ['DB_PASSWORD'],
+        'HOST': os.environ['DB_HOST'],
+        'PORT': os.environ['DB_PORT']
     }
 }
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.postgresql_psycopg2',
-#         'NAME': 'floweryroad',
-#         'USER': 'floweryroad',
-#         'PASSWORD': '1234',
-#         'HOST': 'db',
-#         'PORT': '5432',
-#     }
-# }
-
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
-MEDIA_URL = 'http://127.0.0.1/media/'
+
+MEDIA_URL = 'http://localhost:8000/media/'
