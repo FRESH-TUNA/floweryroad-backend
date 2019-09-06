57ef536d5084a8329c31d4ee7cb9b81ae3a8f309
DONGWON KIM <lunacircle4@likelion.org>
2019-09-06 16:39:06 +0900

    feat: color search bl

--- core/urls.py
+++ core/urls.py
@@ -4,13 +4,13 @@ from rest_framework_nested.routers import NestedSimpleRouter
 from .views import FlowerViewSet, PurposeViewSet, ColorViewSet
 from .views import CommentFlowerViewSet, CommentDeleteViewSet, LikeView
 
-router = DefaultRouter()
+router = DefaultRouter(trailing_slash=False)
 router.register(r'flowers', FlowerViewSet, basename='flower')
 router.register(r'purposes', PurposeViewSet, basename='purpose')
 router.register(r'colors', ColorViewSet, basename='color')
 
 comments_router = NestedSimpleRouter(router, r'flowers', lookup='flower')
-comments_router.register(r'comments', CommentFlowerViewSet, base_name='flower-comments')
+comments_router.register(r'comments', CommentFlowerViewSet)
 
 urlpatterns = [
     path('', include(router.urls)),
